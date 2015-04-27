a2dc612076eaf823dd028d351c16a71141792afd
bryanyang0528 <kenshin2004528@hotmail.com>
2015-04-27 20:13:01 +0800

    fixed gqr bug

--- geocodequery.py
+++ geocodequery.py
@@ -33,8 +33,11 @@ class GeocodeQueryReverse:
     def __init__(self, lat =None, lng = None, language='en', region=None):
         url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng={0},{1}&language={2}'.format(lat, lng, language)
         response = urllib2.urlopen(url)
-        self.jsonResponse =Q json.loads(response.read())
+        self.jsonResponse = json.loads(response.read())
 
     def get_country(self):
         if len(self.jsonResponse["results"]) is not 0:
-            return self.jsonResponse["results"][0]["address_components"][6]["long_name"]
+            for item in self.jsonResponse["results"][0]["address_components"]:
+                if item["types"][0] == "country":
+                    return item["long_name"]
+
